9ff6d44a999d6ac81a37045b5bf560ae2c91dd1d
井上 岳大 <takehiro.agile@gmail.com>
2014-01-11 16:31:27 +0900

    学習テストの追加

--- src/test/java/learning/MathTest.java
+++ src/test/java/learning/MathTest.java
@@ -16,4 +16,39 @@ public class MathTest {
         // Verify
         assertThat(actual, is(expected));
     }
+
+    @Test
+    public void absにマイナス1を渡すと1が返される() throws Exception {
+        // SetUp
+        int expected = 1;
+        // Exercise
+        int actual = Math.abs(-1);
+        // Verify
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void absに0を渡すと0が返される() throws Exception {
+        assertThat(Math.abs(0), is(0));
+    }
+
+    @Test
+    public void absにintの最大値を渡すとintの最大値が返される() throws Exception {
+        // SetUp
+        int expected = Integer.MAX_VALUE;
+        // Exercise
+        int actual = Math.abs(Integer.MAX_VALUE);
+        // Verify
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void absにintの最小値を渡すとintの最小値が返される() throws Exception {
+        // SetUp
+        int expected = Integer.MIN_VALUE;
+        // Exercise
+        int actual = Math.abs(Integer.MIN_VALUE);
+        // Verify
+        assertThat(actual, is(expected));
+    }
 }
